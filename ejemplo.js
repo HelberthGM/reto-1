5e138e9e93a6f429cef13064a3184184cee410df
Helberth Alejandro <helberth12@gmail.com>
2021-10-21 18:53:09 -0500

    cambio de nombres de los html

---
+++ ejemplo.js
@@ -0,0 +1,13 @@
+document.write("Hola mundo");
+
+document.getElementById("test").innerHTML = "Info agregada desde JS:";
+
+document.getElementById("ej").innerHTML = 3 + 7;
+
+alert("Funciona");
+
+console.log("Hola mundo");
+
+function date() {
+    document.getElementById("date").innerHTML = Date();
+}
